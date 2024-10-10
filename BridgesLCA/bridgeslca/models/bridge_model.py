5035e950a11714ecdba744c203fe7b6ddae2e664
mrctnl <mmerictunali@gmail.com>
2024-10-10 12:51:39 +0300

    pulled folder from master branch

--- BridgesLCA/bridgeslca/models/bridge_model.py
+++ BridgesLCA/bridgeslca/models/bridge_model.py
@@ -5,15 +5,15 @@ Created on Tue Oct  8 17:50:06 2024
 
 @author: thibault.chevilliet
 """
+# %%
 import pandas as pd
-
-
 from datetime import date
 from pydantic import BaseModel
 from typing import Optional, NamedTuple
 
 import bridgeslca.models.input_data as data
 
+# %%
 # import sentier_data_tools as sdt
 data.all_data
 
@@ -34,9 +34,9 @@ class Demand(BaseModel):
     amount: float
     spatial_context: IRI = IRI(ref="https://sws.geonames.org/6295630/")
     temporal_range: SimpleDataRange  # TBD
-    lenght: float
-    weight: float
-    tolerance: float
+    length: float
+    width: float
+    tolerance: Optional[float] | 0.2
 
 
 class RunConfig(BaseModel):
@@ -44,16 +44,10 @@ class RunConfig(BaseModel):
     num_samples: int = 1000
 
 
-def within_interval(ref_value: float, targe_value: float, tolerance: float) -> bool:
-    if (ref_value < targe_value * (1 + tolerance)) and (
-        ref_value > targe_value * (1 - tolerance)
-    ):
-        return True
-    else:
-        return False
-
-
-within_interval(3, 3, 0.5)
+def within_interval(ref_value: float, target_value: float, tolerance: float) -> bool:
+    return (ref_value < target_value * (1 + tolerance)) & (
+        ref_value > target_value * (1 - tolerance)
+    )
 
 
 class SentierModel:
@@ -61,78 +55,77 @@ class SentierModel:
         self.demand = demand
 
     def get_model_data(
-        self, demand: Demand
+        self, all_data: dict, reported_technology: dict
     ) -> list[pd.DataFrame]:  # Duck typing also fine
-        if self.demand.product_iri.ref in data.all_data.keys():
-            inputs_df = data.all_data[self.demand.product_iri.ref]
+        self.all_data = all_data
+        self.reported_technology = reported_technology
 
-        else:
-            print(" no DF")
+    def check_tolerance(self) -> list:
 
-        return inputs_df
-
-    def check_tolerance(self) -> str:
-        for row in data.reported_technology.index:
-
-            if within_interval(
-                self.demand.weight,
-                data.reported_technology.loc[row, "weight"],
-                self.demand.tolerance,
-            ) and within_interval(
-                self.demand.lenght,
-                data.reported_technology.loc[row, "lenght"],
-                self.demand.tolerance,
-            ):
-                print("OK -->", row)
-                return row
+        return [
+            row
+            for row in self.reported_technology.index
+            if (
+                within_interval(
+                    self.demand.width,
+                    self.reported_technology.loc[row, "width"],
+                    self.demand.tolerance,
+                )
+                & within_interval(
+                    self.demand.length,
+                    self.reported_technology.loc[row, "length"],
+                    self.demand.tolerance,
+                )
+            )
+        ]
 
     def prepare(self) -> None:
         self.get_model_data()
         self.data_validity_checks()
         self.resample()
 
-    # try to insert check in the existing df
-    def run(self) -> list[Demand]:
-        if isinstance(self.check_tolerance(), str):
-            return data.reported_technology.loc[self.check_tolerance(), :]
+    def make_the_bridge(self) -> dict:
+        custom_bridge = {}
+        custom_bridge["foundation"] = self.demand.width / 2
+        custom_bridge["piles"] = int(self.demand.length / 50)
+        print(custom_bridge)
+        return custom_bridge
 
+    # try to insert check in the existing df
+    def run(self, list_uri: dict) -> list[Demand]:
+
+        if len(self.check_tolerance()) > 0:
+            return self.reported_technology.loc[self.check_tolerance(), :]
         else:
-            print("no matching df")
+            custom_bridge = self.make_the_bridge()
+            for card, parts in enumerate(custom_bridge.keys()):
+                uri_of_flow = list_uri[parts]
+                input_df = custom_bridge[parts] * data.all_data[uri_of_flow]
+
+                input_df
+                if card == 0:
+                    final_df = input_df.copy()
+                else:
+                    fianl_df = pd.concat([final_df, input_df], axis=0)
+
+            return fianl_df
+
+            # print("no matching df")
             # return self.demand.amount * self.get_model_data(self.demand)
 
 
-list_uri = {
-    "Cement": "http://data.europa.eu/xsp/cn2024/cement",
-    "Steel": "http://data.europa.eu/xsp/cn2024/steel",
-}
+# %%
+
 D = Demand(
     product_iri=IRI(ref="http://data.europa.eu/xsp/cn2024/911440000080"),
     properties=None,
     amount=2.0,
     temporal_range=(date(2000, 1, 1), date(2010, 1, 1)),
-    weight=200,
-    lenght=710,
-    tolerance=0.10,
+    width=200,
+    length=1710,
+    tolerance=0.20,
 )
 m = SentierModel(demand=D, run_config=RunConfig())
-m.run()
-m.check_tolerance()
-
-for col in m.run():
-
-    new_D = Demand(
-        product_iri=IRI(ref=list_uri[col]),
-        properties=None,
-        amount=m.run().loc[0, col],
-        temporal_range=(date(2000, 1, 1), date(2010, 1, 1)),
-    )
-    m_new = SentierModel(demand=new_D, run_config=RunConfig())
-    print(m_new.run())
-
-
-m.get_model_data()
-foundation = pd.DataFrame({"Cement": [1], "Steel": [10]})
-
-D.product_iri.ref
-
-data.list_uri
+m.get_model_data(data.all_data, data.reported_technology)
+m.run(data.list_uri)
+# m.check_tolerance()
