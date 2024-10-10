27ddedb5fe04eefcc2514f3377732687d3ab7712
Stefano MRC <stefano.merciai@lca-net.com>
2024-10-10 22:29:48 +0200

    added bonsai vocab

--- BridgesLCA/bridgeslca/models/create_vocab.py
+++ BridgesLCA/bridgeslca/models/create_vocab.py
@@ -16,6 +16,13 @@ from rdflib.namespace import RDF, SKOS
 from rdflib import Literal
 from os.path import dirname
 
+path_home = os.getcwd()
+
+path_repo = os.path.join(path_home, "Departier_repo")
+path_folder = os.path.join(path_repo, "BridgesLCA/BridgesLCA/vocab")
+os.path.exists(path_folder)
+path_file = os.path.join(path_folder, "bonsai.ttl")
+
 
 # bonsai does not have URI yet (here it is just invented)
 bonsai_db_uri = Graph()  # dictionary
@@ -33,8 +40,15 @@ bonsai_db_uri.add(
 bonsai_db_uri.add(
     (
         cement_uri,  # Subject
-        SKOS.relatedMatch,
+        SKOS.closeMatch,
         URIRef("http://data.europa.eu/ehl/cpa21/235"),
+    )
+)
+(
+    (
+        cement_uri,  # Subject
+        SKOS.closeMatch,
+        URIRef("http://data.europa.eu/ehl/cpa21/236"),
     )
 )
 bonsai_db_uri.add(
@@ -44,29 +58,29 @@ bonsai_db_uri.add(
         Literal("Production of cement, lime and plaster, market of", lang="en"),
     )
 )
-bonsai_db_uri.add((cement_uri, SKOS.notation, Literal("M_CMNT", lang="en")))  # Subject
+bonsai_db_uri.add((cement_uri, SKOS.notation, Literal("M_CMNT", lang="en")))
 
 
 # steel
-cement_uri = URIRef("https://lca.aau.dk/FootprintAnalyser/M_STEL")
+steel_uri = URIRef("https://lca.aau.dk/FootprintAnalyser/M_STEL")
 
 bonsai_db_uri.add(
     (
-        cement_uri,  # Subject
+        steel_uri,  # Subject
         RDF.type,
         SKOS.Concept,
     )
 )
 bonsai_db_uri.add(
     (
-        cement_uri,  # Subject
-        SKOS.relatedMatch,
-        URIRef("http://data.europa.eu/ehl/cpa21/241"),
+        steel_uri,  # Subject
+        SKOS.closeMatch,
+        URIRef("http://data.europa.eu/ehl/cpa21/24"),
     )
 )
 bonsai_db_uri.add(
     (
-        cement_uri,  # Subject
+        steel_uri,  # Subject
         SKOS.definition,
         Literal(
             "Production of basic iron and steel and of ferro-alloys and first products thereof, market of",
@@ -74,4 +88,97 @@ bonsai_db_uri.add(
         ),
     )
 )
-bonsai_db_uri.add((cement_uri, SKOS.notation, Literal("M_STEL", lang="en")))  # Subject
+
+bonsai_db_uri.add((steel_uri, SKOS.notation, Literal("M_STEL", lang="en")))
+
+# sand and steel
+sand_uri = URIRef("https://lca.aau.dk/FootprintAnalyser/M_SDCL")
+
+bonsai_db_uri.add(
+    (
+        sand_uri,  # Subject
+        RDF.type,
+        SKOS.Concept,
+    )
+)
+bonsai_db_uri.add(
+    (
+        sand_uri,  # Subject
+        SKOS.closeMatch,
+        URIRef("http://data.europa.eu/ehl/cpa21/0812"),
+    )
+)
+bonsai_db_uri.add(
+    (
+        sand_uri,  # Subject
+        SKOS.definition,
+        Literal(
+            "Sand and clay, market of",
+            lang="en",
+        ),
+    )
+)
+
+bonsai_db_uri.add((sand_uri, SKOS.notation, Literal("M_SDCL", lang="en")))
+
+# bitumen
+bitumen_uri = URIRef("https://lca.aau.dk/FootprintAnalyser/M_BITU")
+
+bonsai_db_uri.add(
+    (
+        bitumen_uri,  # Subject
+        RDF.type,
+        SKOS.Concept,
+    )
+)
+bonsai_db_uri.add(
+    (
+        bitumen_uri,  # Subject
+        SKOS.relatedMatch,
+        URIRef("http://data.europa.eu/ehl/cpa21/192042"),
+    )
+)
+bonsai_db_uri.add(
+    (
+        bitumen_uri,  # Subject
+        SKOS.definition,
+        Literal(
+            "Bitumen, market of",
+            lang="en",
+        ),
+    )
+)
+
+bonsai_db_uri.add((bitumen_uri, SKOS.notation, Literal("M_BITU", lang="en")))
+
+# stone
+stone_uri = URIRef("https://lca.aau.dk/FootprintAnalyser/M_STON")
+
+bonsai_db_uri.add(
+    (
+        stone_uri,  # Subject
+        RDF.type,
+        SKOS.Concept,
+    )
+)
+bonsai_db_uri.add(
+    (
+        stone_uri,  # Subject
+        SKOS.closeMatch,
+        URIRef("http://data.europa.eu/ehl/cpa21/0811"),
+    )
+)
+bonsai_db_uri.add(
+    (
+        stone_uri,  # Subject
+        SKOS.definition,
+        Literal(
+            "Stone, market of",
+            lang="en",
+        ),
+    )
+)
+bonsai_db_uri.add((stone_uri, SKOS.notation, Literal("M_STON", lang="en")))
+
+# export
+bonsai_db_uri.serialize(destination=path_file)
